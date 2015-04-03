ff9128f836604d3bc6c4de188d9ff8b65b33dabe
Venkata Reddy Bhavanam <venuandnani@gmail.com>
2015-04-03 18:07:31 +0530

    add js validation

--- public/js/url.js
+++ public/js/url.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("#tiny_url_form").submit(function() {
     var input = $("#tiny_url_input").val()
+    if(input == "" || input == undefined) {
+      $("#no_input_error").text("Please provide tiny url")
+      $("#no_input_error").show();
+      $('#long_url_container').text('');
+      return false;
+    }
     $.ajax({
       url: '/api/urls?url=' + input,
       method: 'GET',
       dataType: 'json',
       beforeSend: function() {
+        $("#no_input_error").hide();
         $("#loading_spinner").show();
         $('#long_url_container').text('');
       },
